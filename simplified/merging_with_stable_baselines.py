82166baf61e6dd78ae95a37af558ab4ba4ce5ec0
Sharma-Pranav <31562848+Sharma-Pranav@users.noreply.github.com>
2023-02-26 16:15:38 +0100

    Further Progress

--- simplified/merging_with_stable_baselines.py
+++ simplified/merging_with_stable_baselines.py
@@ -1,29 +1,97 @@
-from over_simplified_env_check import *
-
-#from stable_baselines3.common.policies import MlpPolicy
-#from stable_baselines3.common.vec_env import DummyVecEnv
-#from stable_baselines3 import A2C
-
-
-#from stable_baselines3.sac.policies import MlpPolicy
-from stable_baselines.deepq.policies import MlpPolicy
+import os
+from tqdm import tqdm
+#from over_simplified_env_check import *
 from stable_baselines.common.vec_env import DummyVecEnv
-#from stable_baselines3 import A2C, DQN
-
 from stable_baselines.deepq.policies import MlpPolicy, CnnPolicy
 from stable_baselines import DQN
-env = DummyVecEnv([lambda: CustomEnv(4)])
-#env = CustomEnv(4)    
-#check_env(env)
+from over_simplified_env_check import CustomEnv
+from env_run import get_all_task_kubernetes
+from collections import Counter
+path = os.path.join(os.getcwd(), 'Data', '2023_02_06_data', 'data_2.json')
+def get_rewards_kubernetes_data(path, model, env):
+    
+    result_list,_ = get_all_task_kubernetes(path)
 
-Episodes = 1000
-Episode_length = 100
+    [type_list, start_time_list, end_time_list, cpu_list, mem_list] =result_list
+    print('env.train : ', env.train)
+    
+    total_reward = 0
+    action_list = []
+    for i in range(len(start_time_list)):
+        task = [type_list[i], start_time_list[i], end_time_list[i], cpu_list[i], mem_list[i]]
+        env.update_incoming_task(task)
+        observation = env.master.get_master_observation_space()
+        
+        action, _states = model.predict(observation)
+        action_list.append(action)
+        #if i%50==0:
+        #    print('observation : ' , observation)
+        #    print('Kubernetese action_produced : ', action )
+        #if i == 2000:
+        #    break
+        #print(action)
+        #action = env.get_random_action()
+        observation, reward, done, info = env.step(action)
+        total_reward += reward
+    print('set(action_list) : ', set(action_list))   
+    values, counts = Counter(action_list).keys(), Counter(action_list).values()
+    #print('Counter : ', Counter)
+    print('values, counts', values, counts) 
+    print('final observation : ', observation)
+    return total_reward
+    
+#env = DummyVecEnv([lambda: CustomEnv(4)])
+env = CustomEnv(4, train=True)
+Episodes = 10
+Training_cycles = 1
+Episode_length = 10000000
 total_reward_list = []
 
-model = DQN(MlpPolicy, env, verbose=1)
+model = DQN(MlpPolicy, env, verbose=1,exploration_fraction = 0.1,
+            train_freq=10, batch_size =10000, double_q = False,
+            exploration_initial_eps=1.0,  
+            gamma=0.99,exploration_final_eps=0.2,  prioritized_replay=True,
+            learning_rate=1,  buffer_size=5000000)
 
-model.learn(total_timesteps=100000)
+avg_total_reward_kuberenetes  = get_rewards_kubernetes_data(path, model, env)
+print('avg_total_reward_kuberenetes for episode Before : ', avg_total_reward_kuberenetes)
+
+for epi in tqdm(range(Episodes)):
+    env.set_train_param(True)
+    model.learn(total_timesteps=Episode_length)
+    #print('done dqn learn')
+    #print(f'Episode : {epi} reward for episode {sum(env.reward_list )}')
+    env.reward_list = []
+env.set_train_param(False)
+avg_total_reward_kuberenetes  = get_rewards_kubernetes_data(path, model, env)
+print('avg_total_reward_kuberenetes for episode  : ', avg_total_reward_kuberenetes)
+
+
+#print('Evaluating kubernetes rewards : ')
+#env.set_train_param(False)
+#avg_total_reward_kuberenetes  = get_rewards_kubernetes_data(path, model, env,)
+#print('avg_total_reward_kuberenetes Before Training  : ', avg_total_reward_kuberenetes)
+'''
+for episode in range(Episodes):
+    print()
+    print()
+    print()
+    print('episode : ', episode)
+    for cycle in range(Training_cycles):
+        env.set_train_param(True)
+        print(f'Episode : {episode} Training Cycle : {cycle}')
+        model.learn(total_timesteps=Episode_length)
+        print('model.gamma: ', model.gamma)
+        print('model.gamma: ', model.gamma)
+    print('model learning done, know evaluating on Kubenetes data')
+    env.set_train_param(False)
+    avg_total_reward_kuberenetes  = get_rewards_kubernetes_data(path, model, env)
+    print('avg_total_reward_kuberenetes for episode  : ', avg_total_reward_kuberenetes)
 print('done dqn learn')
+
+'''
+
+
 '''
 
 for i in range(Episodes):
--- simplified/over_simplified_env_check.py
+++ simplified/over_simplified_env_check.py
@@ -20,7 +20,7 @@ class Node:
         self.mem = mem
     
     def get_current_state_data(self):
-        state = [self.cpu, self.mem,]
+        state = [self.cpu, self.mem]
         return state
     def update_state(self, cpu_val, mem_val):
         self.cpu = self.cpu + cpu_val
@@ -30,14 +30,15 @@ class Master:
     """[This class serves as framework for definition of Master Node with properties such as 
     task queue, cpu processing, memory, done and undone tasks, Kind of tasks done and undone. all task index]
     """
-    def __init__(self, number_of_nodes):
+    def __init__(self, number_of_nodes, train = True):
         self.number_of_nodes = number_of_nodes
         
         self.max_available_cpu= 4000
         self.max_available_memory = 2
         self.current_incoming_task = [0, 0, 0, 0, 0]
         self.req_cpu = 100
-        self.req_mem = 1
+        self.req_mem = 0.1
+        self.train = train
         self.set_node_list()
         self.action_space = len(self.node_list)
         self.observation_space_dims = self.get_master_observation_space().shape
@@ -45,11 +46,57 @@ class Master:
     def set_node_list(self):
         cpu_params= []
         mem_params= []
+        check = False
         # To be triggered Every Time Step:#
         for _ in range(self.number_of_nodes):
+          '''
+          condition_no = [0, 1, 2, 3, 4]
+          choice = np.random.choice(condition_no)
+          if choice == 0:
+            req_cpu = np.random.randint(0, self.max_available_cpu)
+            req_mem = np.random.uniform(0, self.max_available_memory)
+          elif choice == 1:
+            req_cpu = np.random.randint(self.req_cpu, self.max_available_cpu)
+            req_mem = np.random.uniform(self.req_mem, self.max_available_memory)
+          elif choice == 2:
+            req_cpu = np.random.randint(self.req_cpu, self.max_available_cpu)
+            req_mem = np.random.uniform(0, self.req_mem)
+          elif choice == 3:
+            if self.req_cpu ==0:
+              req_cpu = self.req_cpu
+            else:
+              req_cpu = np.random.randint(0, self.req_cpu)
+            req_mem = np.random.uniform(self.req_mem, self.max_available_memory)
+          elif choice == 4:
+            if self.req_cpu ==0:
+              req_cpu = self.req_cpu
+            else:
+              req_cpu = np.random.randint(0, self.req_cpu)
+            req_mem = np.random.uniform(0, self.req_mem)
+          #print('updating node list')
+          cpu_params.append(req_cpu)#np.random.randint(0, req_cpu))
+          mem_params.append(req_mem)#np.random.uniform(0, req_mem))
+          '''
+          if self.train:
             cpu_params.append(np.random.randint(0, self.max_available_cpu))
             mem_params.append(np.random.uniform(0, self.max_available_memory))
-            #print(cpu_params, mem_params)
+            '''
+            #if np.random.uniform(0, 1)>0.5:
+            if np.random.uniform(0, 1)<0.5:
+              cpu_params.append(np.random.randint(0, self.max_available_cpu))
+              mem_params.append(np.random.uniform(0, self.max_available_memory))
+            else:
+              cpu_params.append(self.max_available_cpu)
+              mem_params.append(self.max_available_memory)
+            #else:
+            #    cpu_params.append(self.max_available_cpu)
+            #    mem_params.append(self.max_available_memory)
+            '''
+          elif self.train == False:
+            #print('elif self.train == False : ', self.train == False)
+            cpu_params.append(self.max_available_cpu)
+            mem_params.append(self.max_available_memory)
+          #print(cpu_params, mem_params)
         self.node_list = [Node(cpu = cpu_params[i], mem = mem_params[i]) for i in range(self.number_of_nodes)]
     
     def set_incoming_task(self, task):
@@ -60,7 +107,8 @@ class Master:
     def get_master_observation_space(self):
         master_observation_space = [] 
         for i, node in  enumerate(self.node_list):
-            master_observation_space.append(node.get_current_state_data())
+            state_normalisation = node.get_current_state_data()
+            master_observation_space.append((state_normalisation[0]/self.max_available_cpu, state_normalisation[1]/ self.max_available_memory))
         master_observation_space = np.vstack(master_observation_space)
         return master_observation_space
     
@@ -70,13 +118,24 @@ class Master:
     def execute_action(self, action):
         reward = -1
         node = self.node_list[action]
+        
+        #if self.train == True:
+        #  print('phase train: ', self.train)
+        #  print('Action Requested', action )
+        #  print('Before State : ', node.get_current_state_data())
+        #  print('self.req_cpu, self.req_mem : ', self.req_cpu, self.req_mem)
         if node.cpu>=self.req_cpu and node.mem >= self.req_mem:
-          #print('Before State : ', node.get_current_state_data())
-          #print('self.req_cpu, self.req_mem : ', self.req_cpu, self.req_mem)
+          
           node.update_state(cpu_val = - self.req_cpu, mem_val= - self.req_mem)
-          #print('After State : ', node.get_current_state_data())
+          
           reward =  1    
-        #print('reward : ', reward)
+        #if self.train == True:
+        #  print('After State : ', node.get_current_state_data())
+        #  print('reward : ', reward)
+        #  print()
+        #  print()
+        #if reward == 1 and self.train:
+        #  a=b
         return reward 
 
     def reset(self):
@@ -112,7 +171,7 @@ class CustomEnv(gym.Env):
   """Custom Environment that follows gym interface"""
   #metadata = {'render.modes': ['human']}
 
-  def __init__(self, number_of_nodes ):
+  def __init__(self, number_of_nodes, train ):
     super(CustomEnv, self).__init__()
     self.number_of_nodes=number_of_nodes
     self.master = Master(number_of_nodes)
@@ -126,6 +185,13 @@ class CustomEnv(gym.Env):
     self.observation_space = Box(low=0.0, high=4000.0, shape=self.master.observation_space_dims, dtype=np.float32)
     #self.observation_space = MultiDiscrete(nvec= )
     #print('self.action_space , self.observation_space : ', self.action_space , self.observation_space)
+    self.train = train
+    self.reward_list = []
+    
+  def set_train_param(self, param):
+    self.train = param
+    self.master.train = param
+    self.master.set_node_list()
     
   def reset(self):
     self.master.reset()
@@ -136,23 +202,31 @@ class CustomEnv(gym.Env):
   def get_random_action(self):
       return self.master.get_random_action()
   def generate_random_task(self):
-    req_cpu = np.random.randint(0, self.master.max_available_cpu)
-    req_mem = np.random.uniform(0, self.master.max_available_memory)
+    req_cpu = np.random.randint(0, self.master.max_available_cpu/4)
+    req_mem = np.random.uniform(0, self.master.max_available_memory/8)
     task = [0,0,0, req_cpu, req_mem]
     return task
 
   def step(self, action):
+    
     reward = self.master.execute_action(action)
+    
+    self.reward_list.append(reward)  
     observation = self.master.get_master_observation_space()
+    #if self.train:
+    #  print('action training : ', action )
+    #  print('observation : ', observation)
+    #  print('reward training : ',  reward)
     #print('observation : ', observation[:,0], observation[:,2])
     done =False
     info = {}
     # Only done during training inference on reall data requires it to be removed
     
-    self.reset()    
-    task = self.generate_random_task()
-    self.update_incoming_task(task)
-    
+    if self.train:
+      #print('inside train : ')
+      task = self.generate_random_task()
+      self.update_incoming_task(task)
+      self.reset()    
     
     return observation, reward, done, info
     
@@ -174,9 +248,6 @@ total_reward_list = []
 for i in range(Episodes):
     total_reward = 0
     for episode_step in range(Episode_length):
-        env.reset()
-        task = env.generate_random_task()
-        env.update_incoming_task(task)
         action = env.get_random_action()
         observation, reward, done, info = env.step(action)
         total_reward += reward
