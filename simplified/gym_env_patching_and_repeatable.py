af91bd7a5c53a8fe208e6ca4ca35c12385b24aa8
Sharma-Pranav <31562848+Sharma-Pranav@users.noreply.github.com>
2023-04-04 10:16:06 +0200

    Work on this leave everything before

--- simplified/gym_env_patching_and_repeatable.py
+++ simplified/gym_env_patching_and_repeatable.py
@@ -11,12 +11,14 @@ class Node:
     """[This class serves as framework for definition of Edge Node with properties such as 
     task queue, service_list, cpu processing and  memory]
     """
-    def __init__(self, cpu:float, mem:float):
+    def __init__(self, cpu:float, mem:float, max_cpu, max_mem):
         self.cpu = cpu
         self.mem = mem
+        self.max_cpu = max_cpu
+        self.max_mem = max_mem
     
     def get_current_state_data(self):
-        state = [self.cpu, self.mem]
+        state = [self.cpu, self.mem, self.max_cpu, self.max_cpu]
         return state
     def update_state(self, cpu_val, mem_val):
         self.cpu = self.cpu + cpu_val
@@ -48,26 +50,15 @@ class Master:
         
         cpu_params= []
         mem_params= []
-        
-        fail_params_choice_list = [[[0, self.req_cpu],[0, self.req_mem]],
-                                [[self.req_cpu, self.max_available_cpu],[0, self.req_mem]],
-                                [[0, self.req_cpu],[self.req_mem, self.max_available_memory],
-                                ]]
-                                
-        
-        fail_params_choice_list = [[[0, self.max_available_cpu],[0, self.max_available_memory]],]
-        selection_list = [i for i in range(self.number_of_nodes)]
-        actionable_node = np.random.choice(selection_list)
-        
-        # To be triggered Every Time Step:#
-        for _ in range(self.number_of_nodes):
-          selected_fail_param = choice(fail_params_choice_list)
-          cpu_params.append(np.random.randint(selected_fail_param[0][0],selected_fail_param[0][1]))
-          mem_params.append(np.random.uniform(selected_fail_param[1][0],selected_fail_param[1][1]))
+        max_cpu_params = [ random.choice(self.max_available_cpu_choices) for i in range(self.number_of_nodes)]
+        max_mem_params = [ random.choice(self.max_available_mem_choices) for i in range(self.number_of_nodes)]
 
-        self.node_list = [Node(cpu = cpu_params[i], mem = mem_params[i]) for i in range(self.number_of_nodes)]
-        self.action_value = actionable_node
-        
+        for i in range(self.number_of_nodes):
+          cpu_params.append(np.random.randint(0,max_cpu_params[i]))
+          mem_params.append(np.random.uniform(0,max_mem_params[i]))
+
+        self.node_list = [Node(cpu = cpu_params[i], mem = mem_params[i], max_cpu=max_cpu_params[i], max_mem=max_mem_params[i]) for i in range(self.number_of_nodes)]
+
     def set_incoming_task(self, task):
         self.current_incoming_task = task
         self.req_cpu = task[3]
@@ -78,10 +69,10 @@ class Master:
         for i, node in  enumerate(self.node_list):
             state_normalisation = node.get_current_state_data()
             if normalised:
-                master_observation_space.append((state_normalisation[0], self.max_available_cpu, state_normalisation[1], self.max_available_memory))
+                master_observation_space.append((state_normalisation[0], state_normalisation[1] , state_normalisation[2], state_normalisation[3]))
             else:
-                master_observation_space.append((state_normalisation[0], self.max_available_cpu, state_normalisation[1], self.max_available_memory))
-        
+                master_observation_space.append((state_normalisation[0], state_normalisation[1] , state_normalisation[2], state_normalisation[3]))
+        master_observation_space.append((self.current_incoming_task[3], self.current_incoming_task[4], 0,0))
         master_observation_space = np.vstack(master_observation_space)
         return master_observation_space
     def get_random_action(self):
@@ -102,31 +93,24 @@ class Master:
         cpu_index_max = max(range(len(cpu_list)), key=cpu_list.__getitem__)  
         mem_index_max = max(range(len(mem_list)), key=mem_list.__getitem__)  
         
-        cpu_reward = (node_choice.cpu/ self.req_cpu)#/self.max_available_cpu)#((node.cpu- self.req_cpu)/self.max_available_cpu)
-        mem_reward = (node_choice.mem/ self.req_mem)#((node.mem- self.req_mem)/self.max_available_memory)
+        cpu_reward = (node_choice.cpu/ self.req_cpu)
+        mem_reward = (node_choice.mem/ self.req_mem)
         
-        #print()
-        #print('action : ', action)
-        #print('Before cpu_reward, mem_reward : ', cpu_reward, mem_reward)
         if cpu_reward>=1 and mem_reward>=1:
           if cpu_reward<mem_reward:
               if action ==cpu_index_max:
-                #cpu_reward = mem_reward
                 cpu_reward = cpu_reward + 10
                 
           elif mem_reward<cpu_reward:
               if action ==mem_index_max:
-                #mem_reward = cpu_reward
                 mem_reward = mem_reward + 10
 
         elif cpu_reward<1 and  mem_reward<1:
           cpu_reward = -abs(cpu_reward)
           mem_reward = -abs(mem_reward)
         else:
-          cpu_reward = -10
-          mem_reward = -10
-        #print('After cpu_reward, mem_reward : ', cpu_reward, mem_reward)  
-        #print('cpu_reward, mem_reward : ', cpu_reward, mem_reward)
+          cpu_reward = -5
+          mem_reward = -5
         reward = cpu_reward  + mem_reward
         '''
         if mem_reward>0:
@@ -155,30 +139,30 @@ class Master:
 
 class CustomEnv(gym.Env):
   """Custom Environment that follows gym interface"""
-  #metadata = {'render.modes': ['human']}
 
   def __init__(self, number_of_nodes, mask_nodes, data, train ):
     super(CustomEnv, self).__init__()
     self.number_of_nodes=number_of_nodes
     self.master = Master(number_of_nodes, data, train)
     self.mask_nodes = mask_nodes
-    
+    self.step_counter = 0
     self.reset()
     self.action_space = spaces.Discrete(self.master.action_space)
 
     self.observation_space = Box(low=0.0, high=4000.0, shape=self.master.observation_space_dims, dtype=np.float32)
     self.train = train
     self.reward_list = []
-    self.step_counter = 0
+    
     self.number_of_masked_nodes = choice([i for i in range(int(self.number_of_nodes/2))])
     task = self.generate_new_task()
     self.update_incoming_task(task) 
     self.data_len = len(self.master.data[0][:])
+
   def set_train_param(self, param):
     self.train = param
     self.master.train = param
     self.master.set_node_list()
-    
+  
   def valid_action_mask(self):
       actions = [i for i in range(self.number_of_nodes)]
       mask = sample(actions,  self.mask_nodes)
@@ -187,7 +171,6 @@ class CustomEnv(gym.Env):
   def reset(self):
     self.master.reset()
     observation = self.master.get_master_observation_space()
-    self.step_counter = 0
     task = self.generate_new_task()
     self.update_incoming_task(task) 
     return observation 
@@ -195,13 +178,13 @@ class CustomEnv(gym.Env):
   def get_random_action(self):
       return self.master.get_random_action()
     
-  '''
+  
   def generate_random_task(self):
     req_cpu = np.random.randint(0, self.master.max_available_cpu/4)
     req_mem = np.random.uniform(0, self.master.max_available_memory/8)
     task = [0,0,0, req_cpu, req_mem]
     return task
-  '''
+
   def generate_new_task(self):
     data = self.master.data
     task = [data[0][self.step_counter], data[1][self.step_counter], data[2][self.step_counter], data[3][self.step_counter], data[4][self.step_counter]]
@@ -212,15 +195,11 @@ class CustomEnv(gym.Env):
 
     done =False
     info = {}
-    # Only done during training inference on reall data requires it to be removed
-    #reward = reward#*self.step_counter/ self.data_len#- self.step_counter*self.data_len
     self.reward_list.append(reward)  
     self.step_counter = self.step_counter + 1
-    self.master.reset()
+    observation_ = self.reset()
     if self.step_counter == self.data_len -1:
         self.step_counter = 0
-
-    observation_ = self.master.get_master_observation_space()
     return observation_, reward, done, info
     
   def update_incoming_task(self, task):
--- simplified/masked_ppo.py
+++ simplified/masked_ppo.py
@@ -17,21 +17,22 @@ def mask_fn(env: gym.Env) -> np.ndarray:
 
 path = os.path.join(os.getcwd(), 'Data', '2023_02_06_data', 'data_2.json')
 result_list,_ = get_all_task_kubernetes(path)
-custom_env = CustomEnv(10, 0, result_list, True)   # Initialize env
+custom_env = CustomEnv(10, 4, result_list, True)   # Initialize env
 customenv = ActionMasker(custom_env, mask_fn)  # Wrap to enable masking
 Episode_length = len(result_list[0])#[:5])
-Episodes = 50#5000
+Episodes = 10#5000
 # MaskablePPO behaves the same as SB3's PPO unless the env is wrapped
 # with ActionMasker. If the wrapper is detected, the masks are automatically
 # retrieved and used when learning. Note that MaskablePPO does not accept
 # a new action_mask_fn kwarg, as it did in an earlier draft.
 total_reward_list = []
 
-model = MaskablePPO(MaskableActorCriticPolicy, customenv, verbose=1, tensorboard_log="tensorboard_logs")#, verbose=True)
+model = MaskablePPO(MaskableActorCriticPolicy, customenv, verbose=0, tensorboard_log="tensorboard_logs")#, verbose=True)
 
 
 
 # Simple one shot training 
+'''
 customenv.set_train_param(True)
 model.learn(total_timesteps=(Episode_length-1)*Episodes)
 print('done PPO2 learn')
@@ -42,23 +43,23 @@ print('done PPO2 learn throughput: ', sum_reward/ (Episode_length))
 model.save(os.path.join('models','PPO2', str(sum_reward)))
 max_reward_model = sum_reward
 total_reward_list.append(sum_reward)
+'''
 
 
-
-# max_reward_model = 0
-# for epi in tqdm(range(Episodes)):
-#     customenv.set_train_param(True)
-#     model.learn(total_timesteps=(Episode_length-1))
-#     print('done PPO2 learn')
-#     print(' len(customenv.reward_list) : ', sum(customenv.reward_list ), len(customenv.reward_list))
-#     sum_reward = sum(customenv.reward_list )/ len(customenv.reward_list)
-#     print(f'Episodes : {epi} reward for episode {sum_reward}')
-#     print('done PPO2 learn throughput: ', sum_reward/ (Episode_length))
-#     if sum_reward>max_reward_model:
-#         model.save(os.path.join('models','PPO2', str(sum_reward)))
-#         max_reward_model = sum_reward
-#     total_reward_list.append(sum_reward)
-    
+max_reward_model = 0
+for epi in tqdm(range(Episodes)):
+    customenv.set_train_param(True)
+    model.learn(total_timesteps=(Episode_length-1))
+    print('done PPO2 learn')
+    print(' len(customenv.reward_list) : ', sum(customenv.reward_list ), len(customenv.reward_list))
+    sum_reward = sum(customenv.reward_list )/ len(customenv.reward_list)
+    print(f'Episodes : {epi} reward for episode {sum_reward}')
+    print('done PPO2 learn throughput: ', sum_reward/ (Episode_length))
+    if sum_reward>max_reward_model:
+        model.save(os.path.join('models','PPO2', str(sum_reward)))
+        max_reward_model = sum_reward
+    total_reward_list.append(sum_reward)
+   
 print('total_reward_list : ', total_reward_list)
 model.save("PPO2")
 plt.plot(total_reward_list)
